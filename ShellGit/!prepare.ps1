6d001daf301b91569b4305ee5a110281af25055f
Dominik Janiec <dominikjaniec@gmail.com>
2018-03-11 23:21:35 +0100

    ShellGit: ConEmu's initial configuration

--- ShellGit/!prepare.ps1
+++ ShellGit/!prepare.ps1
@@ -14,6 +14,16 @@ function EnsureGitAvailable {
     }
 }
 
+function CheckConEmuInstalled {
+    $path = "C:\Program Files\ConEmu\ConEmu64.exe"
+    $installed = Test-Path $path
+    if (-not($installed)) {
+        Write-Output "`n>> Could not find 'ConEmu', configuration skipped..."
+    }
+
+    return $installed
+}
+
 function PrepareEnvironment {
     Write-Output "`n>> Linking Git configuration file to Home directory:"
     MakeHardLinkTo $Env:USERPROFILE $PSScriptRoot ".gitconfig"
@@ -21,6 +31,11 @@ function PrepareEnvironment {
     Write-Output "`n>> Linking Bash configuration files to Home directory:"
     MakeHardLinkTo $Env:USERPROFILE $PSScriptRoot ".bash_profile"
     MakeHardLinkTo $Env:USERPROFILE $PSScriptRoot ".bashrc"
+
+    if (CheckConEmuInstalled) {
+        Write-Output "`n>> Linking ConEmu configuration file to 'AppData` directory:"
+        MakeHardLinkTo $Env:APPDATA $PSScriptRoot "ConEmu.xml"
+    }
 }
 
 function HardLinkConfigBack {
@@ -30,6 +45,11 @@ function HardLinkConfigBack {
     Write-Output "`n>> Linking Bash configurations file from Home directory:"
     MakeHardLinkTo $PSScriptRoot $Env:USERPROFILE ".bash_profile" $false
     MakeHardLinkTo $PSScriptRoot $Env:USERPROFILE ".bashrc" $false
+
+    if (CheckConEmuInstalled) {
+        Write-Output "`n>> Linking ConEmu configuration file from 'AppData` directory:"
+        MakeHardLinkTo $PSScriptRoot $Env:APPDATA "ConEmu.xml" $false
+    }
 }
 
 #######################################################################################
